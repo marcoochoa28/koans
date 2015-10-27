537c4e9ea3cbe90627ef002c6b405a62cd15a45f
marcoochoa28 <marcoochoa28@hotmail.com>
2015-10-26 18:32:27 -0600

    commit 6

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -31,6 +31,30 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
   # You need to write this method
+  score = 0
+  counts = Hash.new(0) 
+  dice.each do |x|
+    counts[x] += 1
+  end
+
+  (1..6).each do |i|
+    if counts[i] >= 3 
+      if i == 1
+        score += 1000
+      else
+        score += 100 * i
+      end
+
+      counts[i] = [counts[i] - 3, 0].max
+    end
+
+    if i == 1
+      score += 100 * counts[i]
+    elsif i == 5
+      score += 50 * counts[i]
+    end
+  end
+  score
 end
 
 class AboutScoringProject < Neo::Koan
